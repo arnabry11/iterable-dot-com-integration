8d8695d68c7e6e787b6e95fe3f943a5c40d581eb
ARNAB ROY <35267739+arnabry11@users.noreply.github.com>
2024-07-28 17:06:54 +0530

    Associate events and users using join table (#10)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
 
+  has_and_belongs_to_many :events # rubocop:disable Rails/HasAndBelongsToMany
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
