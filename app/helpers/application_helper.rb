58bd2a6113e893aed3d9d6d5f78c395ce7f584d5
Arnab <rarnab021@gmail.com>
2024-07-28 19:17:42 +0530

    Add styles to the application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def flash_message_prefix(message_type)
+    case message_type
+    when 'success' then t('alert_prefix.success')
+    when 'info'    then t('alert_prefix.info')
+    when 'warning' then t('alert_prefix.warning')
+    when 'danger'  then t('alert_prefix.danger')
+    else ''
+    end
+  end
+
+  def message_type(message_type)
+    case message_type
+    when 'notice' then t('message_type.notice')
+    when 'alert'  then t('message_type.alert')
+    else message_type
+    end
+  end
+
 end
