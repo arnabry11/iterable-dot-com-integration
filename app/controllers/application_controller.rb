5667f16cf2bbf1d08e7a36177d4cfce6667eae20
ARNAB ROY <35267739+arnabry11@users.noreply.github.com>
2024-07-29 09:09:40 +0530

    Send nice error message for 500 errors (#18)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  rescue_from StandardError do
+    render text: 'Internal server error.', status: :internal_server_error
+  end
+
 end
